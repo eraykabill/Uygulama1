39319b2d912e88b23549eedab98077b1964bbe92
eraykabill <m.eraykabil@gmail.com>
2022-09-15 14:32:52 +0300

    Uygulama1

---
+++ Uygulama1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir Butonu");
+            }
+            Console.ReadLine();
+        }
+    }
+}
